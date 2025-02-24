2e20971faca4c95e3f16e1afd18921dc82c1e6cc
Leoforio-blatb <blatbelmonte@ims.tu-darmstadt.de>
2025-02-24 09:45:40 +0100

    Refactored some folder naming in ./charging_software/...

--- examples/charging_software/main.py
+++ examples/charging_software/main.py
@@ -12,22 +12,14 @@ from graphrag_sdk import KnowledgeGraph, Ontology
 
 logging.basicConfig(level=logging.INFO)
 
-folder = "ext-switchev-iso15118"  # "everest-core"
+folder = "some_iso_norm"  # "everest-core"
 path_in = "examples/charging_software/03_data_in/code_repos"
 # path_in = "examples/charging_software/03_data_in/code_repos"
 
-MODEL_TYPE = "groq"  # Switch to "ollama", "litellm" 
+MODEL_TYPE = "gemini"  # Switch to "ollama", "litellm" 
 
 if __name__ == "__main__":
-    # Source configuration: Data folder.
-    src_files = path_in+"/"+folder
-    sources = []
-
-    # For each file in the source directory and its subdirectories, create a new Source object.
-    for root, dirs, files in os.walk(src_files):
-        for file in files:
-            sources.append(Source(os.path.join(root, file)))
-        # Model configuration
+    ###################### SELECT LLM MODEL#### ########################
     if MODEL_TYPE == "litellm":
         model = LiteModel(model_name="deepseek/deepseek-chat")
         # model = LiteModel(model_name="deepseek/deepseek-reasoner")
@@ -37,19 +29,30 @@ if __name__ == "__main__":
         model = LiteModel(model_name="openrouter/deepseek/deepseek-r1:free")
     elif MODEL_TYPE == "ollama":
         model = OllamaGenerativeModel(model_name="deepseek-r1:14b")
-        # "deepseek-r1:14b"
     elif MODEL_TYPE == "gemini":
         model = GeminiGenerativeModel(model_name="models/gemini-2.0-flash-exp")
     else:
         logging.info("Specify LLM to be used.")
     
-    # # Generate ontology
-    f_name = "ontology_"+folder+".json"
+    ################### CREATE ONTOLOGIES FROM SOURCES ########################
+    # # Source configuration: Data folder.
+    # src_files = path_in+"/"+folder
+    # sources = []
 
-    generate_ontology(sources, model, f_name)
+    # # For each file in the source directory and its subdirectories, create a new Source object.
+    # for root, dirs, files in os.walk(src_files):
+    #     for file in files:
+    #         sources.append(Source(os.path.join(root, file)))
 
-    # ontologies_dir = "examples"+"/"+"charging_software"+"/"+"05_ontologies"
-    # target_dir = "examples"+"/"+"charging_software"
+    # # # Generate ontology
+    # f_name = "ontology_"+folder+".json"
+
+    # generate_ontology(sources, model, f_name)
+
+    ######################### MERGE JSON ONTOLOGIES ###########################
+
+    # ontologies_dir = "examples"+"/"+"charging_software"+"/"+"04_ontologies"
+    # target_dir = "examples"+"/"+"charging_software"+"/"+"05_merged_ontologies"
 
     # merged = merge_ontology_directory(ontologies_dir)
 
@@ -57,26 +60,33 @@ if __name__ == "__main__":
     # with open(os.path.join(target_dir, "merged_ontology.json"), "w") as f:
     #     json.dump(merged.to_json(), f, indent=2)
 
-    # with open(os.path.join(target_dir, "merged_ontology.json"), "r") as f:
-    #     ontology = Ontology.from_json(json.loads(f.read()))
-    
-    # # Build knowledge graph with unified config
-    # kg = KnowledgeGraph(
-    #     name="charging-demo",
-    #     model_config=KnowledgeGraphModelConfig.with_model(model),
-    #     ontology=ontology,
-    #     host="localhost",
-    #     port=6379
-    # )
-    
-    # kg.process_sources(sources)
+    ########### CREATE KNOWLEDGE GRAPH FROM (MERGED) ONTOLOGIES ###############
 
-    # # Example usage
-    # graph = build_knowledge_graph("redis://localhost:6379", ontology)
+    ontologies_dir = "examples"+"/"+"charging_software"+"/"+"04_ontologies"
+    fname_onto = "ontology_citrineos.json"
 
-    # # Add chat interface from quickstart
-    # def query_kg(question: str):
-    #     chat = kg.chat_session()
-    #     return chat.send_message(question)
+    with open(os.path.join(ontologies_dir, fname_onto), "r") as f:
+        ontology = Ontology.from_json(json.loads(f.read()))
     
-    # print(query_kg("Explain key EV charging concepts in this knowledge base"))
+    # Build knowledge graph with unified config
+    kg = KnowledgeGraph(
+        name="charging-demo",
+        model_config=KnowledgeGraphModelConfig.with_model(model),
+        ontology=ontology,
+        host="localhost",
+        port=6379
+    )
+    
+    # # # kg.process_sources(sources)  # Only if directly from sources to kg.
+
+    # Example usage
+    graph = build_knowledge_graph("redis://localhost:6379", ontology)
+
+    ########### INTERACT WITH KNOWLEDGE GRAPH, SAVE IT AS .rdb file ###########
+
+    # Add chat interface from quickstart
+    def query_kg(question: str):
+        chat = kg.chat_session()
+        return chat.send_message(question)
+    
+    print(query_kg("Explain key EV charging concepts in this knowledge base"))
